adb41387fd62b82a966688135a90f3729e992ba6
suyashbhalla <suyash.bhalla88@gmail.com>
2020-06-02 01:01:55 +0530

    leetcode_invert_tree

---
+++ src/LC/challenge/june/InvertTree.java
@@ -0,0 +1,34 @@
+package LC.challenge.june;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+        return root;
+    }
+}
